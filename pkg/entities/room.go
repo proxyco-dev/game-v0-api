9ccb1d134af9dc061dcad80cdf94e2498bc0e012
Nika Shamiladze <shamgoingzuck@gmail.com>
2025-02-23 18:36:15 +0400

    feat: Add CreatedBy field to Room entity
    - Add CreatedBy and CreatedByID fields to Room struct
    - Establish a foreign key relationship with the User who created the room
    - Enhance room entity to track room creation metadata

--- pkg/entities/room.go
+++ pkg/entities/room.go
@@ -7,14 +7,16 @@ import (
 )
 
 type Room struct {
-	ID         uuid.UUID  `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
-	Users      []*User    `json:"users" gorm:"many2many:room_users;"`
-	Title      string     `json:"title"`
-	IsActive   bool       `json:"isActive" gorm:"default:true"`
-	MaxPlayers int        `json:"maxPlayers" gorm:"default:2"`
-	IsDeleted  bool       `json:"isDeleted" gorm:"default:false"`
-	CreatedAt  *time.Time `json:"createdAt" gorm:"column:createdAt"`
-	UpdatedAt  *time.Time `json:"updatedAt" gorm:"column:createdAt"`
+	ID          uuid.UUID  `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
+	Users       []*User    `json:"users" gorm:"many2many:room_users;"`
+	Title       string     `json:"title"`
+	IsActive    bool       `json:"isActive" gorm:"default:true"`
+	MaxPlayers  int        `json:"maxPlayers" gorm:"default:2"`
+	CreatedBy   User       `json:"createdBy" gorm:"foreignKey:CreatedByID"`
+	CreatedByID uuid.UUID  `json:"createdById" gorm:"type:uuid"`
+	IsDeleted   bool       `json:"isDeleted" gorm:"default:false"`
+	CreatedAt   *time.Time `json:"createdAt" gorm:"column:createdAt"`
+	UpdatedAt   *time.Time `json:"updatedAt" gorm:"column:createdAt"`
 }
 
 func (Room) TableName() string {
