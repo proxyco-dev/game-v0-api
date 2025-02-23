ada5eb11e3d8277931919a4b881f5f32dcae116e
Nika Shamiladze <shamgoingzuck@gmail.com>
2025-02-23 18:27:32 +0400

    feat: Add max players field to Room entity
    - Add MaxPlayers field to Room struct with default value of 2
    - Update Room entity to support configuring maximum number of players per room

--- pkg/entities/room.go
+++ pkg/entities/room.go
@@ -7,13 +7,14 @@ import (
 )
 
 type Room struct {
-	ID        uuid.UUID  `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
-	Users     []*User    `json:"users" gorm:"many2many:room_users;"`
-	Title     string     `json:"title"`
-	IsActive  bool       `json:"isActive" gorm:"default:true"`
-	IsDeleted bool       `json:"isDeleted" gorm:"default:false"`
-	CreatedAt *time.Time `json:"createdAt" gorm:"column:createdAt"`
-	UpdatedAt *time.Time `json:"updatedAt" gorm:"column:createdAt"`
+	ID         uuid.UUID  `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
+	Users      []*User    `json:"users" gorm:"many2many:room_users;"`
+	Title      string     `json:"title"`
+	IsActive   bool       `json:"isActive" gorm:"default:true"`
+	MaxPlayers int        `json:"maxPlayers" gorm:"default:2"`
+	IsDeleted  bool       `json:"isDeleted" gorm:"default:false"`
+	CreatedAt  *time.Time `json:"createdAt" gorm:"column:createdAt"`
+	UpdatedAt  *time.Time `json:"updatedAt" gorm:"column:createdAt"`
 }
 
 func (Room) TableName() string {
